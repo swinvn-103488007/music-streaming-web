3cbc8939627f4c9879eefc05abf8e99fd25bed5a
Luu Dao Dung Tri <luudaodungtri@gmail.com>
2023-07-10 01:48:24 +0700

    add custom color

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,20 @@
 <template>
-  <!-- <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/> -->
-  <div class="container">
-    <div class="row"></div>
+  <div class="container bg-app-bg">
+    <div class="row">
+      <NavigationTab></NavigationTab>
+    </div>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import  NavigationTab  from '@/components/NavigationTab.vue'
 
-nav {
-  padding: 30px;
+export default {
+  name: 'App',
+  components: {
+    NavigationTab
+  }
 }
+</script>
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
 
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
--- src/components/NavigationTab.vue
+++ src/components/NavigationTab.vue
@@ -1,5 +1,19 @@
 <template>
     <div class="col-3">
-        
+        <div>
+            <Home fillColor="#FF7E3A"/>
+            <span class="text-info">  Home</span>
+        </div>
     </div>
 </template>
+
+<script>
+import Home from 'vue-material-design-icons/Home.vue'
+
+export default {
+  name: 'NavigationTab',
+  components: {
+    Home
+  }
+}
+</script>
