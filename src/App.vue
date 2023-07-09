397d23762542ee8cc8c6c109b822a9c63823e5c1
Luu Dao Dung Tri <luudaodungtri@gmail.com>
2023-07-09 21:46:29 +0700

    add font and navigation bar file

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
-  <nav>
+  <!-- <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
-  <router-view/>
+  <router-view/> -->
+  <div class="container">
+    <div class="row"></div>
+  </div>
 </template>
 
 <style>
