5a5d070b462cd1165c5d86c84a6cc78fd17cbfea
Luu Dao Dung Tri <luudaodungtri@gmail.com>
2023-08-10 10:57:29 +0700

    delete unecessary css

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import '@/bootstrap/css/custom.css'
+// import '@/bootstrap/css/custom.css'
+import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.bundle'
 import mitt from 'mitt'
 
